1f2397bedd14c7aa5318660215c701aff6481a02
shaka <jaherm65@gmail.com>
2021-10-27 08:05:30 +0600

    pull added

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
 const port = 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json())
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8ztnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
@@ -18,15 +24,12 @@ async function run(){
 
         //POST API
         app.post('/services', async(req, res) =>{
-            const service = {
-                "name": "ENGINE DIAGNOSTIC",
-                "price": "300",
-                "description": "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
-                "img": "https://i.ibb.co/dGDkr4v/1.jpg"
-            }
+            const service = req.body;
+            console.log('hit the post api', service);
 
             const result = await serviecesCollection.insertOne(service);
             console.log(result)
+            res.send(result)
         })
     }
     finally{
@@ -43,4 +46,3 @@ app.get('/', (req, res) => {
 app.listen(port, () =>{
     console.log('Runnig Genius Server on Port', port)
 })
-
